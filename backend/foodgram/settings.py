a2d40b623812c3c5148457555afbdfe45ec75b02
Konstantin-Kleinikov <konstantinkleinikov@gmail.com>
2025-07-27 10:39:01 +0300

    1.367 Fix error with staticfiles.

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
