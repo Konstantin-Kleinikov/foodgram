cfa0c160b2d3c46ecdc792af992ea9dc624b9a54
Konstantin-Kleinikov <konstantinkleinikov@gmail.com>
2025-07-28 13:32:18 +0300

    1.52 Modify XML generation to use measurement unit as grouping criteria.

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -26,16 +26,58 @@ def decode_base62(short_code):
 
 
 def create_shopping_cart_xml(user, ingredients):
+    """
+        Создает XML-представление корзины покупок с группировкой ингредиентов
+        по названию и единице измерения.
+
+        Параметры:
+            user (User): Объект пользователя, для которого формируется корзина
+            ingredients (dict): Словарь ингредиентов, где ключ -
+                                объект Ingredient, а значение -
+                                количество ингредиента
+
+        Возвращает:
+            str: Форматированную XML-строку с данными корзины покупок
+
+        Структура выходного XML:
+        <ShoppingCart>
+            <User name="Имя пользователя" date="ДД.ММ.ГГГГ">
+                <Ingredient>
+                    <Name>Название ингредиента</Name>
+                    <Amount>Количество</Amount>
+                    <MeasurementUnit>Единица измерения</MeasurementUnit>
+                </Ingredient>
+                ...
+            </User>
+        </ShoppingCart>
+
+        Особенности работы:
+        - Ингредиенты группируются по названию и единице измерения
+        - Количества одинаковых ингредиентов суммируются
+        - Дата формируется в формате ДД.ММ.ГГГГ
+        - XML выводится в отформатированном виде с отступами
+        """
+    grouped_ingredients = {}
+    for ingredient, amount in ingredients.items():
+        key = (ingredient.name, ingredient.measurement_unit)
+        if key in grouped_ingredients:
+            grouped_ingredients[key] += amount
+        else:
+            grouped_ingredients[key] = amount
+
     root = Element('ShoppingCart')
     user_element = SubElement(root, 'User')
     user_element.set('name', user.get_full_name())
     user_element.set('date', timezone.now().strftime('%d.%m.%Y'))
 
-    for ingredient, amount in ingredients.items():
+    for (name, measurement_unit), total_amount in grouped_ingredients.items():
         ingredient_element = SubElement(user_element, 'Ingredient')
-        SubElement(ingredient_element, 'Name').text = ingredient
-        SubElement(ingredient_element, 'Amount').text = str(amount)
-        SubElement(ingredient_element, 'MeasurementUnit').text = 'г.'
+        SubElement(ingredient_element, 'Name').text = name
+        SubElement(ingredient_element, 'Amount').text = str(total_amount)
+        SubElement(
+            ingredient_element,
+            'MeasurementUnit'
+        ).text = measurement_unit
 
     return prettify(root)
 
